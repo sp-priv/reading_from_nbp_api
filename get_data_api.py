1580c9a0dcdb999cc862b7ed92e374a7ff24cb28
sp-priv <sebastian.pietrzak@gmail.com>
2019-11-25 13:57:55 +0100

    add bid / ask price, add validation of currency, delete json in request parameter, added script's arguments

--- get_data_api.py
+++ get_data_api.py
@@ -1,25 +1,40 @@
-from pandas.io.json import json_normalize
 import requests
+import re
+import argparse
+
+URL = "http://api.nbp.pl/api/exchangerates/rates/c/{code}/today/"
 
 
-def get_currency():
-    code = input("Type currency that you would like to to exchange rate for (3 characters) \n")
-    code = code.lower()
+def get_args():
+    my_parser = argparse.ArgumentParser(description="Argument parser for currency code")
+    my_parser.add_argument('--code',
+                           action='store', type=str, required=True, help='Currency code - 3 letters long string')
+    args = my_parser.parse_args()
+    code = args.code
+    code = str(code).lower()
+    if not re.match(r'^[a-z]{3}$',code):
+        exit('Currency code is not valid. It has to be 3 letters long. Also please make sure it follows '
+             'ISO 4217: https://pl.wikipedia.org/wiki/ISO_4217'
+             ' \n')
     return code
 
 
 def get_ex_rate():
-    code = get_currency()
-    table = 'a'
-    # code = 'chf'
-    URL = "http://api.nbp.pl/api/exchangerates/rates/{table}/{code}/?format=json".format(table=table,code=code)
-    r = requests.get(url = URL)
+    global URL
+    code = get_args()
+    r = requests.get(url=URL.format(code=code),headers={'Accept': 'application/json'})
+    if r.status_code == 404:
+        exit('Invalid currency code or table is not published yet.'
+             'Please stick to ISO 4217: https://pl.wikipedia.org/wiki/ISO_4217')
     data = r.json()
-    rates = json_normalize(data['rates'])
-    ex_rate = rates['mid'][0]
-    print(f'1 {code.upper()} equals {ex_rate} in PLN')
-    return ex_rate
+    ex_rate_bid = round(data['rates'][0]['bid'], 2)
+    ex_rate_ask = round(data['rates'][0]['ask'], 2)
+    print(f'1 {code.upper()} bid price is {ex_rate_bid} PLN \n1 {code.upper()} ask price is {ex_rate_ask} '
+          f'PLN \n ')
 
 
 if __name__ == '__main__':
-    get_ex_rate() 
+    try:
+        get_ex_rate()
+    except Exception as e:
+        print("type error: " + str(e))
